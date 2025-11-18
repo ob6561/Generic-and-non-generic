211c718cecae95d1bf535ed7737e2243ee97ea31
Om_Bandyopadhyay <ombandyopadhyay@gmail.com>
2025-11-18 12:31:05 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Generic_and_non_generic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
